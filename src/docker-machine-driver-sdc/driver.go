553199b167cea8ee6e13cd954e616f32a933e5ad
Tianon Gravi <admwiggin@gmail.com>
2016-04-21 15:50:30 -0700

    Add more goodies (including and especially configurable "image" and "package" values)

--- src/docker-machine-driver-sdc/driver.go
+++ src/docker-machine-driver-sdc/driver.go
@@ -4,10 +4,12 @@ import (
 	"errors"
 	"fmt"
 	"io/ioutil"
-	"log"
+	stdlog "log"
 	"os"
 
 	"github.com/docker/machine/libmachine/drivers"
+	"github.com/docker/machine/libmachine/engine"
+	"github.com/docker/machine/libmachine/log"
 	"github.com/docker/machine/libmachine/mcnflag"
 	"github.com/docker/machine/libmachine/state"
 
@@ -18,6 +20,8 @@ import (
 
 const (
 	driverName = "sdc"
+	flagPrefix = driverName + "-"
+	envPrefix  = "SDC_"
 )
 
 var (
@@ -45,29 +49,44 @@ type Driver struct {
 	SdcKeyId   string
 	SdcUrl     string
 
+	// machine creation parameters
+	SdcImage   string
+	SdcPackage string
+
 	// machine state
 	SdcMachineId string
 }
 
 // SetConfigFromFlags configures the driver with the object that was returned by RegisterCreateFlags
 func (d *Driver) SetConfigFromFlags(opts drivers.DriverOptions) error {
-	d.SdcAccount = opts.String(driverName + "-account")
-	d.SdcKeyFile = opts.String(driverName + "-key-file")
-	d.SdcKeyId = opts.String(driverName + "-key-id")
-	d.SdcUrl = opts.String(driverName + "-url")
+	d.SdcAccount = opts.String(flagPrefix + "account")
+	d.SdcKeyFile = opts.String(flagPrefix + "key-file")
+	d.SdcKeyId = opts.String(flagPrefix + "key-id")
+	d.SdcUrl = opts.String(flagPrefix + "url")
+
+	d.SdcImage = opts.String(flagPrefix + "image")
+	d.SdcPackage = opts.String(flagPrefix + "package")
+
 	d.SetSwarmConfigFromFlags(opts)
 
 	if d.SdcAccount == "" {
-		return fmt.Errorf("%s driver requires the --%s-account/SDC_ACCOUNT option", driverName, driverName)
+		return fmt.Errorf("%s driver requires the --%saccount/%sACCOUNT option", driverName, flagPrefix, envPrefix)
 	}
 	if d.SdcKeyFile == "" {
-		return fmt.Errorf("%s driver requires the --%s-key-file/SDC_KEY_FILE option", driverName, driverName)
+		return fmt.Errorf("%s driver requires the --%skey-file/%sKEY_FILE option", driverName, flagPrefix, envPrefix)
 	}
 	if d.SdcKeyId == "" {
-		return fmt.Errorf("%s driver requires the --%s-key-id/SDC_KEY_ID option", driverName, driverName)
+		return fmt.Errorf("%s driver requires the --%skey-id/%sKEY_ID option", driverName, flagPrefix, envPrefix)
 	}
 	if d.SdcUrl == "" {
-		return fmt.Errorf("%s driver requires the --%s-url/SDC_URL option", driverName, driverName)
+		return fmt.Errorf("%s driver requires the --%surl/%sURL option", driverName, flagPrefix, envPrefix)
+	}
+
+	if d.SdcImage == "" {
+		return fmt.Errorf("%s driver requires the --%simage option", driverName, flagPrefix)
+	}
+	if d.SdcPackage == "" {
+		return fmt.Errorf("%s driver requires the --%spackage option", driverName, flagPrefix)
 	}
 
 	return nil
@@ -77,28 +96,39 @@ func (d *Driver) SetConfigFromFlags(opts drivers.DriverOptions) error {
 func (d *Driver) GetCreateFlags() []mcnflag.Flag {
 	return []mcnflag.Flag{
 		mcnflag.StringFlag{
-			EnvVar: "SDC_URL",
-			Name:   driverName + "-url",
+			EnvVar: envPrefix + "URL",
+			Name:   flagPrefix + "url",
 			Usage:  "URL of the CloudAPI endpoint",
 			Value:  defaultSdcUrl,
 		},
 		mcnflag.StringFlag{
-			EnvVar: "SDC_ACCOUNT",
-			Name:   driverName + "-account",
+			EnvVar: envPrefix + "ACCOUNT",
+			Name:   flagPrefix + "account",
 			Usage:  "Login name/username",
 			Value:  defaultSdcAccount,
 		},
 		mcnflag.StringFlag{
-			EnvVar: "SDC_KEY_ID",
-			Name:   driverName + "-key-id",
-			Usage:  `The fingerprint of $SDC_KEY_FILE (ssh-keygen -l -E md5 -f $SDC_KEY_FILE | awk '{ gsub(/^[^:]+:/, "", $2); print $2 }')`,
+			EnvVar: envPrefix + "KEY_ID",
+			Name:   flagPrefix + "key-id",
+			Usage:  fmt.Sprintf(`The fingerprint of $%sKEY_FILE (ssh-keygen -l -E md5 -f $%sKEY_FILE | awk '{ gsub(/^[^:]+:/, "", $2); print $2 }')`, envPrefix, envPrefix),
 			Value:  defaultSdcKeyId,
 		},
 		mcnflag.StringFlag{
-			EnvVar: "SDC_KEY_FILE",
-			Name:   driverName + "-key-file",
-			Usage:  "An SSH private key file that has been added to $SDC_ACCOUNT",
+			EnvVar: envPrefix + "KEY_FILE",
+			Name:   flagPrefix + "key-file",
+			Usage:  fmt.Sprintf("An SSH private key file that has been added to $%sACCOUNT", envPrefix),
 			Value:  defaultSdcKeyFile,
+		},
+
+		mcnflag.StringFlag{
+			Name:  flagPrefix + "image",
+			Usage: "VM image to provision",
+			Value: defaultSdcImage,
+		},
+		mcnflag.StringFlag{
+			Name:  flagPrefix + "package",
+			Usage: "VM instance size to create",
+			Value: defaultSdcPackage,
 		},
 	}
 }
@@ -123,7 +153,7 @@ func (d Driver) client() (*cloudapi.Client, error) {
 		creds.SdcEndpoint.URL,
 		cloudapi.DefaultAPIVersion,
 		creds,
-		log.New(os.Stderr, "", log.LstdFlags),
+		stdlog.New(os.Stderr, "", stdlog.LstdFlags),
 	)), nil
 }
 func (d *Driver) getMachine() (*cloudapi.Machine, error) {
@@ -135,6 +165,8 @@ func (d *Driver) getMachine() (*cloudapi.Machine, error) {
 	if err != nil {
 		return nil, err
 	}
+
+	log.Debugf("machine name: %s", machine.Name)
 
 	// update d.IPAddress since we know the value (saves later work)
 	d.IPAddress = machine.PrimaryIP
@@ -169,9 +201,8 @@ func (d *Driver) Create() error {
 	machine, err := client.CreateMachine(cloudapi.CreateMachineOpts{
 		Name: d.MachineName,
 
-		// TODO configurable in some way
-		Image:   defaultSdcImage,
-		Package: defaultSdcPackage,
+		Image:   d.SdcImage,
+		Package: d.SdcPackage,
 	})
 	if err != nil {
 		return err
@@ -208,11 +239,16 @@ func (d *Driver) GetSSHHostname() (string, error) {
 // GetURL returns a Docker compatible host URL for connecting to this host
 // e.g. tcp://1.2.3.4:2376
 func (d *Driver) GetURL() (string, error) {
+	if err := drivers.MustBeRunning(d); err != nil {
+		return "", err
+	}
+
 	ip, err := d.GetIP()
 	if err != nil {
 		return "", err
 	}
-	return fmt.Sprintf("tcp://%s:2376", ip), nil
+
+	return fmt.Sprintf("tcp://%s:%d", ip, engine.DefaultPort), nil
 }
 
 func (d *Driver) GetSSHKeyPath() string {
@@ -231,17 +267,15 @@ func (d *Driver) GetState() (state.State, error) {
 
 	// https://github.com/joyent/smartos-live/blob/master/src/vm/man/vmadm.1m.md#vm-states
 	switch machine.State {
-	case "configured":
+	case "configured", "provisioning":
 		return state.Starting, nil
-	case "installed":
-		return state.Starting, nil
-	case "ready":
-		return state.Starting, nil
+	case "failed", "receiving":
+		return state.Error, nil
 	case "running":
 		return state.Running, nil
-	case "shutting_down":
+	case "shutting_down", "stopping":
 		return state.Stopping, nil
-	case "down":
+	case "down", "stopped":
 		return state.Stopped, nil
 	}
 
@@ -250,11 +284,8 @@ func (d *Driver) GetState() (state.State, error) {
 
 // Kill stops a host forcefully
 func (d *Driver) Kill() error {
-	client, err := d.client()
-	if err != nil {
-		return err
-	}
-	return client.StopMachine(d.SdcMachineId)
+	// TODO find something more forceful than "Stop"
+	return d.Stop()
 }
 
 // Remove a host
@@ -286,6 +317,9 @@ func (d *Driver) Start() error {
 
 // Stop a host gracefully
 func (d *Driver) Stop() error {
-	// TODO
-	return errUnimplemented
+	client, err := d.client()
+	if err != nil {
+		return err
+	}
+	return client.StopMachine(d.SdcMachineId)
 }
